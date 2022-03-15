4857e4c45cc20ee87f36223dddd111e198c7d272
ieh <hodellurt@gmail.com>
2022-03-15 13:30:07 +0900

    feat(theory): theory.finite_character_union, theory.of_finite_induction

--- src/consistency.lean
+++ src/consistency.lean
@@ -8,8 +8,8 @@ variables {L : language.{u}} {T U : theory L}
 namespace theory
 open provable axiomatic_classical_logic'
 
-lemma not_consistent_iff_bot {T : theory L} : ¬T.consistent ↔ T ⊢ ⊥ :=
-by simp[consistent_iff_bot T]
+lemma consistent_of_consistent_ss {T U : theory L} (h : consistent T) (ss : U ⊆ T) : consistent U :=
+by { simp[consistent_iff_bot] at h ⊢, intros hU, have : T ⊢ ⊥, from weakening hU ss, contradiction }
 
 private lemma list_set_finite {α} (l : list α) : {a : α | a ∈ l}.finite :=
 by { induction l with a l IH, { simp },
@@ -40,6 +40,40 @@ begin
   have : ¬s ⊢ ⊥, exact (consistent_iff_bot s).mp this,
   contradiction
 end⟩
+
+lemma finite_character_union (consis : consistent T) :
+  consistent (T ∪ U) ↔ ∀ (s ⊆ U) (f : s.finite), consistent (T ∪ s) :=
+begin
+  rw finite_character, split,
+  { intros h s s_ss s_fin,
+    rw finite_character, intros s' s'_ss, refine h s'
+      (by { have : T ∪ s ⊆ T ∪ U, { simp, exact set.subset_union_of_subset_right s_ss T },
+      exact set.subset.trans s'_ss this }) },
+  { intros h s s_ss s_fin,
+    let u := s ∩ U,
+    have lmm : consistent (T ∪ u), from h u (by simp[u]) (by { simp[u], exact set.finite.inter_of_left s_fin U }),
+    have ss : s ⊆ T ∪ u, { intros p mem, simp[u], have : p ∈ T ∨ p ∈ U, from s_ss mem, tauto },
+    refine consistent_of_consistent_ss lmm ss }
+end
+
+lemma of_finite_induction
+  (consis : consistent T)
+  (H : ∀ (s ⊆ U) (p ∈ U), s.finite → consistent (T ∪ s) → consistent ((T ∪ s) +{ p })) :
+  consistent (T ∪ U) :=
+begin
+  refine (finite_character_union consis).mpr _,
+  suffices : ∀ (s : theory L), set.finite s → s ⊆ U → (T ∪ s).consistent,
+  { intros s ss fin, exact this s fin ss },
+  intros s fin,
+  refine set.finite.induction_on fin (λ _, by simp[consis]) _,
+  intros p s nmem fin consis ss, 
+  have : T ∪ insert p s = (T ∪ s) +{ p }, { ext q, simp },
+  simp[this],
+  exact H s (set.subset.trans (show s ⊆ insert p s, by simp) ss) p (ss (show p ∈ insert p s, by simp)) fin
+    (consis (set.subset.trans (show s ⊆ insert p s, by simp) ss))
+end
+
+
 
 end theory
 
