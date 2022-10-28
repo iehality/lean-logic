f42250159c84003d44370565c194802d84f7b656
jeh <hodellurt@gmail.com>
2022-10-28 14:11:29 +0900

    fixfixfixfix

--- src/PL/pl.lean
+++ src/PL/pl.lean
@@ -1,4 +1,4 @@
-import lib
+import lib.lib
 
 universe u
 
@@ -13,44 +13,45 @@ inductive formula : Type u
 
 variables {A}
 
-instance : has_top (formula A) := ⟨formula.verum⟩
+namespace formula
 
-instance : has_arrow (formula A) := ⟨formula.imply⟩
+def and (p : formula A) (q : formula A) : formula A := (p.imply q.neg).neg
 
-instance : has_negation (formula A) := ⟨formula.neg⟩
+def or (p : formula A) (q : formula A) : formula A := p.neg.imply q
+
+instance : has_logic_symbol (formula A) :=
+{ bot := verum.neg,
+  top := verum,
+  sup := or,
+  inf := and,
+  arrow := imply,
+  neg := neg }
 
 attribute [pattern] has_negation.neg has_arrow.arrow
-
-instance : has_bot (formula A) := ⟨⁻⊤⟩
+end formula
 
 instance : inhabited (formula A) := ⟨⊤⟩
 
-@[simp] lemma formula.top_eq : @formula.verum A = (⊤) := rfl
-@[simp] lemma formula.imply_eq : @formula.imply A = (⟶) := rfl
-@[simp] lemma formula.neg_eq : @formula.neg A = has_negation.neg := rfl
-
 @[reducible] def theory (A : Type u) := set (formula A)
 
-def formula.and (p : formula A) (q : formula A) : formula A := ⁻(p ⟶ ⁻q)
+namespace formula
 
-instance : has_inf (formula A) := ⟨formula.and⟩
+@[simp] lemma top_eq : @formula.verum A = (⊤) := rfl
 
-def formula.or (p : formula A) (q : formula A) : formula A := ⁻p ⟶ q
+@[simp] lemma imply_eq : @formula.imply A = (⟶) := rfl
 
-instance : has_sup (formula A) := ⟨formula.or⟩
+@[simp] lemma neg_eq : @formula.neg A = has_negation.neg := rfl
 
-@[simp] lemma formula.imply.inj' (p₁ q₁ p₂ q₂ : formula A) : p₁ ⟶ p₂ = q₁ ⟶ q₂ ↔ p₁ = q₁ ∧ p₂ = q₂ :=
+@[simp] lemma imply.inj' (p₁ q₁ p₂ q₂ : formula A) : p₁ ⟶ p₂ = q₁ ⟶ q₂ ↔ p₁ = q₁ ∧ p₂ = q₂ :=
 ⟨formula.imply.inj, by simp; exact congr_arg2 (⟶)⟩
 
-@[simp] lemma formula.neg.inj' (p q : formula A) : ⁻p = ⁻q ↔ p = q := ⟨formula.neg.inj, congr_arg _⟩
+@[simp] lemma neg.inj' (p q : formula A) : ⁻p = ⁻q ↔ p = q := ⟨formula.neg.inj, congr_arg _⟩
 
-@[simp] lemma formula.and.inj' (p₁ q₁ p₂ q₂ : formula A) : p₁ ⊓ p₂ = q₁ ⊓ q₂ ↔ p₁ = q₁ ∧ p₂ = q₂ :=
+@[simp] lemma and.inj' (p₁ q₁ p₂ q₂ : formula A) : p₁ ⊓ p₂ = q₁ ⊓ q₂ ↔ p₁ = q₁ ∧ p₂ = q₂ :=
 by simp[has_inf.inf, formula.and]
 
-@[simp] lemma formula.or.inj' (p₁ q₁ p₂ q₂ : formula A) : p₁ ⊔ p₂ = q₁ ⊔ q₂ ↔ p₁ = q₁ ∧ p₂ = q₂ :=
+@[simp] lemma or.inj' (p₁ q₁ p₂ q₂ : formula A) : p₁ ⊔ p₂ = q₁ ⊔ q₂ ↔ p₁ = q₁ ∧ p₂ = q₂ :=
 by simp[has_sup.sup, formula.or]
-
-namespace formula
 
 @[simp] def rew : (A → formula A) → formula A → formula A
 | s (atom a) := s a
